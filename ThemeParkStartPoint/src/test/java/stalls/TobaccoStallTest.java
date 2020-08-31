6c0936f22168a1c92785dbec000f492122606496
peaky76 <robertjamespeacock@gmail.com>
2020-08-31 15:14:55 +0100

    TobaccoStall: implements ISecurity

--- ThemeParkStartPoint/src/test/java/stalls/TobaccoStallTest.java
+++ ThemeParkStartPoint/src/test/java/stalls/TobaccoStallTest.java
@@ -2,16 +2,21 @@ package stalls;
 
 import org.junit.Before;
 import org.junit.Test;
+import people.Visitor;
 
 import static org.junit.Assert.assertEquals;
 
 public class TobaccoStallTest {
 
     TobaccoStall tobaccoStall;
+    Visitor u18;
+    Visitor adult;
 
     @Before
     public void setUp() throws Exception {
         tobaccoStall = new TobaccoStall("Jacks Drum", "Jack Jarvis", ParkingSpot.B1);
+        u18 = new Visitor(17, 180, 30);
+        adult = new Visitor(35, 180, 50);
     }
 
     @Test
@@ -28,4 +33,10 @@ public class TobaccoStallTest {
     public void hasParkingSpot() {
         assertEquals(ParkingSpot.B1, tobaccoStall.getParkingSpot());
     }
+
+    @Test
+    public void willRefuseU18() { assertEquals(false, tobaccoStall.isAllowedTo(u18)); }
+
+    @Test
+    public void willAllowAdults() { assertEquals(true, tobaccoStall.isAllowedTo(adult)); }
 }
