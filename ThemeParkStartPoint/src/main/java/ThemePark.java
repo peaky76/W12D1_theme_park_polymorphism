7eaf29307d1a0f13251b0c6bb920f1d9bf5eff41
peaky76 <robertjamespeacock@gmail.com>
2020-08-31 16:14:08 +0100

    ThemePark: created with list of stalls and attractions

--- ThemeParkStartPoint/src/main/java/ThemePark.java
+++ ThemeParkStartPoint/src/main/java/ThemePark.java
@@ -1,2 +1,36 @@
+import attractions.Attraction;
+import stalls.Stall;
+
+import java.util.ArrayList;
+
 public class ThemePark {
+
+//ThemePark stores all Attractions and Stalls in it.
+//ThemePark has a method getAllReviewed() which returns an ArrayList of IReviewed objects.
+//Add an empty visitedAttractions ArrayList to Visitor which stores a list of Attractions and a method to add an attraction to the list.
+//ThemePark has a method visit(Visitor, Attraction) calls for the attraction to increment its visitCount by 1 and adds the attraction to the visitors visitedAttractions list.
+
+    ArrayList<Attraction> attractions;
+    ArrayList<Stall> stalls;
+
+    public ThemePark() {
+        this.attractions = new ArrayList<Attraction>();
+        this.stalls = new ArrayList<Stall>();
+    }
+
+    public ArrayList<Attraction> getAttractions() {
+        return attractions;
+    }
+
+    public ArrayList<Stall> getStalls() {
+        return stalls;
+    }
+
+    public void add(Attraction attraction) {
+        this.attractions.add(attraction);
+    }
+
+    public void add(Stall stall) {
+        this.stalls.add(stall);
+    }
 }
---
+++ ThemeParkStartPoint/src/test/java/ThemeParkTest.java
@@ -0,0 +1,56 @@
+import attractions.Dodgems;
+import attractions.Park;
+import attractions.Playground;
+import attractions.RollerCoaster;
+import org.junit.Before;
+import org.junit.Test;
+import stalls.CandyflossStall;
+import stalls.IceCreamStall;
+import stalls.ParkingSpot;
+import stalls.TobaccoStall;
+
+import static org.junit.Assert.assertEquals;
+
+public class ThemeParkTest {
+
+    ThemePark themePark;
+    Dodgems dodgems;
+    Park park;
+    Playground playground;
+    RollerCoaster rollerCoaster;
+    CandyflossStall candyflossStall;
+    IceCreamStall iceCreamStall;
+    TobaccoStall tobaccoStall;
+
+    @Before
+    public void setUp() throws Exception {
+        themePark = new ThemePark();
+
+        dodgems = new Dodgems("Bumper Cars", 5);
+        park = new Park("Leafy Meadows", 9);
+        playground = new Playground("Fun Zone", 7);
+        rollerCoaster = new RollerCoaster("Blue Ridge", 10);
+
+        candyflossStall = new CandyflossStall("Candy Land", "Harry Belafonte", ParkingSpot.A1, 5);
+        iceCreamStall = new IceCreamStall("Dream Cones", "Vanilla Ice", ParkingSpot.A4, 8);
+        tobaccoStall = new TobaccoStall("Jacks Drum", "Jack Jarvis", ParkingSpot.B1, 3);
+
+        themePark.add(dodgems);
+        themePark.add(park);
+        themePark.add(playground);
+        themePark.add(rollerCoaster);
+        themePark.add(candyflossStall);
+        themePark.add(iceCreamStall);
+        themePark.add(tobaccoStall);
+    }
+
+    @Test
+    public void hasAttractions() {
+        assertEquals(4, themePark.attractions.size());
+    }
+
+    @Test
+    public void hasStalls() {
+        assertEquals(3, themePark.stalls.size());
+    }
+}
