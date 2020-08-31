c98708e82d932f0ca0b103cecb1a65f5d36af871
peaky76 <robertjamespeacock@gmail.com>
2020-08-31 15:21:36 +0100

    RollerCoaster: implements ISecurity

--- ThemeParkStartPoint/src/test/java/attractions/RollercoasterTest.java
+++ ThemeParkStartPoint/src/test/java/attractions/RollercoasterTest.java
@@ -2,16 +2,24 @@ package attractions;
 
 import org.junit.Before;
 import org.junit.Test;
+import people.Visitor;
 
 import static org.junit.Assert.assertEquals;
 
 public class RollercoasterTest {
 
     RollerCoaster rollerCoaster;
+    Visitor tooshort;
+    Visitor tooyoung;
+    Visitor talladult;
 
     @Before
     public void setUp() {
+
         rollerCoaster = new RollerCoaster("Blue Ridge", 10);
+        tooshort = new Visitor(18, 140, 30);
+        tooyoung = new Visitor(11, 180, 30);
+        talladult = new Visitor( 25, 180, 30);
     }
 
     @Test
@@ -28,4 +36,13 @@ public class RollercoasterTest {
     public void hasVisitCount() {
         assertEquals(0, rollerCoaster.getVisitCount());
     }
+
+    @Test
+    public void willRefuseTooShort() { assertEquals(false, rollerCoaster.isAllowedTo(tooshort)); }
+
+    @Test
+    public void willRefuseTooYoung() { assertEquals(false, rollerCoaster.isAllowedTo(tooyoung)); }
+
+    @Test
+    public void willAllowTallAndOldEnough() { assertEquals(true, rollerCoaster.isAllowedTo(talladult)); }
 }
